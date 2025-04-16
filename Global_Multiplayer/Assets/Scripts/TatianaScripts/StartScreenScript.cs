28cee94fe88497a91b6b052727857c7403880367
Aquamarine444 <miatharmse@gmail.com>
2025-04-16 17:07:20 +0200

    UIOverload | Credits Panel

--- Global_Multiplayer/Assets/Scripts/TatianaScripts/StartScreenScript.cs
+++ Global_Multiplayer/Assets/Scripts/TatianaScripts/StartScreenScript.cs
@@ -6,44 +6,13 @@ using UnityEngine.UI;
 
 public class StartScreenScript : MonoBehaviour
 {
-    public GameObject StartPanel;
-
-    public Sprite StaticScreen;
-    public Sprite FreddieScreen;
-    public Sprite BonnieScreen;
-
     private void Start()
     {
-        StartCoroutine(FirstChange());
+
     }
 
-    IEnumerator FirstChange()
+    public void QuitGame()
     {
-        yield return new WaitForSecondsRealtime(20);
-        StartPanel.GetComponent<Image>().sprite = StaticScreen;
-        StartCoroutine(SecondChange());
+        Application.Quit();
     }
-
-    IEnumerator SecondChange()
-    {
-        yield return new WaitForSecondsRealtime(5);
-        StartPanel.GetComponent<Image>().sprite = FreddieScreen;
-        StartCoroutine(ThirdChange());
-    }
-
-    IEnumerator ThirdChange()
-    {
-        yield return new WaitForSecondsRealtime(20);
-        StartPanel.GetComponent<Image>().sprite = StaticScreen;
-        StartCoroutine(FinalChange());
-    }
-
-    IEnumerator FinalChange()
-    {
-        yield return new WaitForSecondsRealtime(5);
-        StartPanel.GetComponent<Image>().sprite = BonnieScreen;
-        StartCoroutine(FirstChange());
-    }
-
-
 }
