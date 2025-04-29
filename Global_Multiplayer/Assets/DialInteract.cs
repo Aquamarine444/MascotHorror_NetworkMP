8e6ba461a10d7fbe73b76247d197eef490160e96
DKTDeanThomas <deanthomas634@gmail.com>
2025-04-29 03:36:09 +0200

    assembly line

--- Global_Multiplayer/Assets/DialInteract.cs
+++ Global_Multiplayer/Assets/DialInteract.cs
@@ -5,6 +5,7 @@ public class DialInteract : MonoBehaviour, IInteractible
     [SerializeField] private bool inspect;
     [SerializeField] private bool examine;
     [SerializeField] private GameObject examineCamera;
+    [SerializeField] private GameObject particles;
     public bool Inspect { get { return inspect; } }
     public bool Examine { get { return examine; } }
     public GameObject ExamineCam { get { return examineCamera; } }
@@ -15,6 +16,8 @@ public class DialInteract : MonoBehaviour, IInteractible
 
     public bool Interact(Interactors interact)
     {
+
+        Destroy(particles);
         interact.gameObject.GetComponent<CommsManager>().InteractComment(text);
         interact.gameObject.GetComponent<PlayerInventory>().hasDial = true;
         interact.gameObject.GetComponent<PlayerInventory>().UpdateInventory();
